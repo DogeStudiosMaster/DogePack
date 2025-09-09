58253dffda75cca67728ddc6af6efecf8edb47d6
Hexy <hexygaming99@gmail.com>
2025-09-08 22:14:18 -0600

    moved lighting stuff

--- 1.21.6_shaders/assets/minecraft/shaders/core/terrain.vsh
+++ 1.21.6_shaders/assets/minecraft/shaders/core/terrain.vsh
@@ -1,4 +1,20 @@
 #moj_import <fog.glsl>
 #moj_import <dynamictransforms.glsl>
 #moj_import <projection.glsl>
+
+in vec3 Position;
+in vec4 Color;
+in vec2 UV0;
+in ivec2 UV2;
+in vec3 Normal;
+
+uniform sampler2D Sampler2;
+
+out float sphericalVertexDistance;
+out float cylindricalVertexDistance;
+out vec4 vertexColor;
+out vec2 texCoord0;
+out vec4 lightMapColor; // Added missing output
+
+
 #moj_import <light_sample.glsl>
--- 1.21.6_shaders/assets/minecraft/shaders/include/light_sample.glsl
+++ 1.21.6_shaders/assets/minecraft/shaders/include/light_sample.glsl
@@ -1,20 +1,6 @@
 #ifndef _LIGHTMAPSAMPLE_GLSL
 #define _LIGHTMAPSAMPLE_GLSL
 
-in vec3 Position;
-in vec4 Color;
-in vec2 UV0;
-in ivec2 UV2;
-in vec3 Normal;
-
-uniform sampler2D Sampler2;
-
-out float sphericalVertexDistance;
-out float cylindricalVertexDistance;
-out vec4 vertexColor;
-out vec2 texCoord0;
-out vec4 lightMapColor; // Added missing output
-
 void main() {
     vec3 pos = Position + ModelOffset;
     gl_Position = ProjMat * ModelViewMat * vec4(pos, 1.0);
