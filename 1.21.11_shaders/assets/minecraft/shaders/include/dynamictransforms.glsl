94bbb36420dc7ebf6655f4e33a7d40b5d0efeada
Hexy <hexygaming99@gmail.com>
2025-10-12 17:25:02 -0600

    updated shaders for 1.21.11

---
+++ 1.21.11_shaders/assets/minecraft/shaders/include/dynamictransforms.glsl
@@ -0,0 +1,11 @@
+#ifndef _DYNAMICTRANSFORMS_GLSL
+#define _DYNAMICTRANSFORMS_GLSL
+
+layout(std140) uniform DynamicTransforms {
+    mat4 ModelViewMat;
+    vec4 ColorModulator;
+    vec3 ModelOffset;
+    mat4 TextureMat;
+};
+
+#endif // _DYNAMICTRANSFORMS_GLSL
